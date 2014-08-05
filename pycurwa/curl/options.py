0dcb47c170df009cfe4195eae2f1c8e9505d1097
dipstef <dipstef@gmail.com>
2014-08-05 17:14:27 +0100

    refactoring options

--- pycurwa/curl/options.py
+++ pycurwa/curl/options.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import pycurl
 
 
-class SetOptions(object):
+class _setions(object):
 
     def set_network_options(self, interface=None, proxy=None, use_ipv6=False):
         if interface:
@@ -16,93 +16,92 @@ class SetOptions(object):
 
     def set_proxy(self, proxy):
         if proxy.is_socks4():
-            self.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS4)
+            self._set(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS4)
         elif proxy.is_socks5():
-            self.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5)
+            self._set(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5)
         else:
-            self.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_HTTP)
+            self._set(pycurl.PROXYTYPE, pycurl.PROXYTYPE_HTTP)
 
-        self.setopt(pycurl.PROXY, proxy.address)
-        self.setopt(pycurl.PROXYPORT, proxy.port)
+        self._set(pycurl.PROXY, proxy.address)
+        self._set(pycurl.PROXYPORT, proxy.port)
         if proxy.auth:
-            self.setopt(pycurl.PROXYUSERPWD, str('%s:%s' % (proxy.user, proxy.password)))
+            self._set(pycurl.PROXYUSERPWD, str('%s:%s' % (proxy.user, proxy.password)))
 
     def set_interface(self, interface):
-        self.setopt(pycurl.INTERFACE, interface)
+        self._set(pycurl.INTERFACE, interface)
 
     def set_ipv6_resolve(self):
-        self.setopt(pycurl.IPRESOLVE, pycurl.IPRESOLVE_WHATEVER)
+        self._set(pycurl.IPRESOLVE, pycurl.IPRESOLVE_WHATEVER)
 
     def set_ipv4_resolve(self):
-        self.setopt(pycurl.IPRESOLVE, pycurl.IPRESOLVE_V4)
+        self._set(pycurl.IPRESOLVE, pycurl.IPRESOLVE_V4)
 
     def set_auth(self, auth):
-        self.setopt(pycurl.USERPWD, auth)
+        self._set(pycurl.USERPWD, auth)
 
     def set_low_speed_timeout(self, timeout):
-        self.setopt(pycurl.LOW_SPEED_TIME, timeout)
+        self._set(pycurl.LOW_SPEED_TIME, timeout)
 
     def set_cookie(self, cookie):
-        self.setopt(pycurl.COOKIE, cookie)
+        self._set(pycurl.COOKIE, cookie)
 
     def set_cookie_list(self, cookie):
-        self.setopt(pycurl.COOKIELIST, cookie)
-
-    def clear_cookies(self):
-        self.unsetopt(self, pycurl.COOKIELIST)
-
-    def unset_cookie_files(self):
-        self.setopt(pycurl.COOKIEFILE, '')
-        self.setopt(pycurl.COOKIEJAR, '')
+        self._set(pycurl.COOKIELIST, cookie)
 
     def set_url(self, url):
-        self.setopt(pycurl.URL, url)
+        self._set(pycurl.URL, url)
 
     def set_method(self, method):
-        self.setopt(pycurl.CUSTOMREQUEST, method)
+        self._set(pycurl.CUSTOMREQUEST, method)
 
     def set_referrer(self, referrer):
-        self.setopt(pycurl.REFERER, referrer)
+        self._set(pycurl.REFERER, referrer)
 
     def set_headers(self, headers):
-        self.setopt(pycurl.HTTPHEADER, headers)
+        self._set(pycurl.HTTPHEADER, headers)
 
     def set_range(self, start, end=None):
         bytes_range = '%i-%i' % (start, end) if end else '%i-' % start
-        self.setopt(pycurl.RANGE, bytes_range)
+        self._set(pycurl.RANGE, bytes_range)
 
     def set_resume(self, resume):
-        self.setopt(pycurl.RESUME_FROM, resume)
+        self._set(pycurl.RESUME_FROM, resume)
 
     def set_body_fun(self, body):
-        self.setopt(pycurl.WRITEFUNCTION, body)
+        self._set(pycurl.WRITEFUNCTION, body)
 
     def set_header_fun(self, header_fun):
-        self.setopt(pycurl.HEADERFUNCTION, header_fun)
+        self._set(pycurl.HEADERFUNCTION, header_fun)
 
     def set_progress_function(self, fun):
-        self.setopt(pycurl.PROGRESSFUNCTION, fun)
+        self._set(pycurl.PROGRESSFUNCTION, fun)
 
     def headers_only(self):
-        self.setopt(pycurl.NOBODY, 1)
+        self._set(pycurl.NOBODY, 1)
 
     def enable_body_retrieve(self):
-        self.setopt(pycurl.NOBODY, 0)
+        self._set(pycurl.NOBODY, 0)
+
+    def _set(self, name, value):
+        self.setopt(name, value)
 
 
 class GetOptions(object):
 
     def get_response_url(self):
-        return self.getinfo(pycurl.EFFECTIVE_URL)
+        return self._get(pycurl.EFFECTIVE_URL)
 
     def get_cookies(self):
-        return self.getinfo(pycurl.INFO_COOKIELIST)
+        return self._get(pycurl.INFO_COOKIELIST)
 
     def get_status_code(self):
-        return int(self.getinfo(pycurl.RESPONSE_CODE))
+        return int(self._get(pycurl.RESPONSE_CODE))
 
     def get_speed_download(self):
-        return self.getinfo(pycurl.SPEED_DOWNLOAD)
+        return self._get(pycurl.SPEED_DOWNLOAD)
+
+    def _get(self, name):
+        return self.getinfo(name)
 
 
 class Auth(namedtuple('Auth', ['user', 'password'])):
