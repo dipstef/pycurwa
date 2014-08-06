f72debecb47800a7396c4c0713680f0b127b5caa
dipstef <dipstef@gmail.com>
2014-08-06 12:50:11 +0100

    curl multi performance

--- tests/multi/performance/__init__.py
+++ tests/multi/performance/__init__.py
@@ -1,1 +1,15 @@
-__author__ = 'dipstef'
+from contextlib import contextmanager
+from pycurwa.multi import PyCurwaMulti
+
+
+@contextmanager
+def request_times(method, url, times):
+    with PyCurwaMulti() as pycurwa:
+
+        responses = []
+        for i in range(1, times+1):
+            print i
+            response = pycurwa.request(method, url)
+            responses.append(response)
+
+        yield responses
