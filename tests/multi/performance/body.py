cd54bf2653457a977710abce6159882ba8ad4e5a
dipstef <dipstef@gmail.com>
2014-08-06 16:35:41 +0100

    curl multi perfomance test

--- tests/multi/performance/body.py
+++ tests/multi/performance/body.py
@@ -1,1 +1,12 @@
-__author__ = 'dipstef'
+from tests.multi.performance import request_times
+
+
+def main():
+    #don' try this at home, actually it holds fine
+    with request_times('GET', 'http://diskstation:5000', times=10000) as responses:
+        for response in responses:
+            print response.body
+
+
+if __name__ == '__main__':
+    main()
