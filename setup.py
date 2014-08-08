e6e3cc82d14e018229798c9249a22be764ac6274
dipstef <dipstef@gmail.com>
2014-08-08 23:52:18 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+settings = dict()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Topic :: Internet',
+    'Topic :: Utilities',
+]
+
+settings.update(
+    name='pycurwa',
+    version='0.1',
+    description='',
+    long_description=open('README.rst').read(),
+    author='Stefano Dipierro',
+    license='Apache 2.0',
+    url='https://github.com/dipstef/httpy',
+    classifiers=CLASSIFIERS,
+    keywords='http client curl pycurl',
+    packages=[],
+    test_suite='tests',
+    requires=['unicoder', 'httpy']
+)
+
+setup(**settings)
